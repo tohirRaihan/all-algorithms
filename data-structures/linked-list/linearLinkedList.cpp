3515e329df30ade9a93b7b4a048c2dab1f248073
Tohir Raihan <tohir.raihan@gmail.com>
2022-08-09 01:09:27 +0600

    linear linked list

--- data-structures/linked-list/linearLinkedList.cpp
+++ data-structures/linked-list/linearLinkedList.cpp
@@ -5,30 +5,58 @@ class Node {
 public:
   int value;
   Node *Next;
+
+  // constructer creation
+  Node(int val) {
+    this->value = val;
+    this->Next = NULL;
+  }
 };
+
+void insertAtTail(Node *&head, int val) {
+  Node *newNode = new Node(val);
+  if (head == NULL) {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->Next != NULL) {
+    temp = temp->Next;
+  }
+  temp->Next = newNode;
+}
+
+void insertAtHead(Node *&head, int val) {
+  Node *newNode = new Node(val);
+  newNode->Next = head;
+  head = newNode;
+}
 
 void display(Node *n) {
   while (n != NULL) {
-    cout << n->value << endl;
+    cout << n->value << " ";
     n = n->Next;
   }
 }
 
 int main(void) {
-  Node *head = new Node;
-  Node *second = new Node;
-  Node *third = new Node;
-  Node *fourth = new Node;
+  Node *head = NULL;
 
-  head->value = 1;
-  second->value = 2;
-  third->value = 3;
-  fourth->value = 4;
-
-  head->Next = second;
-  second->Next = third;
-  third->Next = fourth;
-  fourth->Next = NULL;
+  int n;
+  cout << "Choice 1: Insert at Head" << endl
+       << "Choice 2: Insert at Tail" << endl
+       << "Choice 3: Exit" << endl;
+  int choice = 2;
+  while (choice == 1 || choice == 2) {
+    cout << "Enter The Value" << endl;
+    cin >> n;
+    if (choice == 1)
+      insertAtHead(head, n);
+    else if (choice == 2)
+      insertAtTail(head, n);
+    cout << "Next Choice: (1/2/3) ?" << endl;
+    cin >> choice;
+  }
 
   display(head);
   return 0;
