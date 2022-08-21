aad5b3b5e9c170a81a21bffea7e793f1ace8764b
Tohir Raihan <tohir.raihan@gmail.com>
2022-08-22 01:53:46 +0600

    update stackBasic

--- data-structures/stack/stackBasic.cpp
+++ data-structures/stack/stackBasic.cpp
@@ -15,27 +15,69 @@ public:
 };
 
 class Stack {
-public:
   Node *head;
   Node *top;
+  int count = 0;
 
+public:
+  Stack() {
+    head = NULL;
+    top = NULL;
+  }
   // PUSH
   void push(int val) {
     Node *newNode = new Node(val);
     if (head == NULL) {
       head = top = newNode;
+      count++;
       return;
     }
 
     top->Next = newNode;
     newNode->Prev = top;
     top = newNode;
-
+    count++;
   }
   // POP
+  int pop() {
+    Node *delNode;
+    delNode = top;
+    int chk;
+    // there is no element in the stack
+    if (head == NULL) {
+      cout << "Stack Underflow" << endl;
+      return -1;
+    }
+    // there is only one element
+    if (top == head) {
+      top = head = NULL;
+    } else { // there is more than one element
+      top = delNode->Prev;
+      top->Next = NULL;
+    }
+    chk = delNode->value;
+    delete delNode;
+    count--;
+    return chk;
+  }
   // EMPTY
+  bool empty() {
+    if (head == NULL) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   // SIZE
+  int size() { return count; }
   // TOP
+  int getTop() {
+    if (top == NULL) {
+      cout << "Stack Underflow | There is no element in top" << endl;
+    } else {
+      return top->value;
+    }
+  }
 };
 
 int main(void) { return 0; }
