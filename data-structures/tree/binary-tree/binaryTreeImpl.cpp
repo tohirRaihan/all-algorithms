fd03572c3a51c20c1a4d17f082aa26f08c7b21fd
Tohir Raihan <tohir.raihan@gmail.com>
2022-09-23 19:30:16 +0600

    binary tree

--- data-structures/tree/binary-tree/binaryTreeImpl.cpp
+++ data-structures/tree/binary-tree/binaryTreeImpl.cpp
@@ -14,4 +14,31 @@ public:
   }
 };
 
-int main() { return 0; }
+void printTree(TreeNode* root, int level) {
+  if (root == NULL) {
+    return;
+  }
+  cout << "Root: " << endl;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  TreeNode *allNodes[n];
+  for (int i = 0; i < n; i++) {
+    allNodes[i]->data = -1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    int value, left, right;
+    cin >> value >> left >> right;
+    allNodes[i]->data = value;
+    if (left != -1) {
+      allNodes[i]->leftChild = allNodes[left];
+    }
+    if (right != -1) {
+      allNodes[i]->rightChild = allNodes[right];
+    }
+  }
+  return 0;
+}
