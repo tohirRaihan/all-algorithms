6120e76f8334cb3ced754a55e922c53ade1735b2
Tohir Raihan <tohir.raihan@gmail.com>
2022-09-24 02:14:06 +0600

    binary tree

--- data-structures/tree/binary-tree/binaryTreeImpl.cpp
+++ data-structures/tree/binary-tree/binaryTreeImpl.cpp
@@ -14,11 +14,39 @@ public:
   }
 };
 
-void printTree(TreeNode* root, int level) {
+void printTree(TreeNode *root, int level);
+void spacePrint(int level);
+
+void printTree(TreeNode *root, int level) {
   if (root == NULL) {
     return;
   }
-  cout << "Root: " << endl;
+  if (root->leftChild == NULL && root->rightChild == NULL) {
+    cout << root->data << endl;
+    return;
+  } else {
+    cout << endl;
+    spacePrint(level);
+    cout << "Root: " << root->data << endl;
+  }
+
+  if (root->leftChild != NULL) {
+    spacePrint(level);
+    cout << "Left: ";
+    printTree(root->leftChild, level + 1);
+  }
+
+  if (root->rightChild != NULL) {
+    spacePrint(level);
+    cout << "Right: ";
+    printTree(root->rightChild, level + 1);
+  }
+}
+
+void spacePrint(int level) {
+  for (int i = 0; i < level; i++) {
+    cout << "   ";
+  }
 }
 
 int main() {
@@ -26,13 +54,17 @@ int main() {
   cin >> n;
   TreeNode *allNodes[n];
   for (int i = 0; i < n; i++) {
-    allNodes[i]->data = -1;
+    allNodes[i] = new TreeNode(-1);
   }
 
   for (int i = 0; i < n; i++) {
     int value, left, right;
     cin >> value >> left >> right;
     allNodes[i]->data = value;
+    if (left > n - 1 || right > n - 1) {
+      cout << "Invalid Index" << endl;
+      break;
+    }
     if (left != -1) {
       allNodes[i]->leftChild = allNodes[left];
     }
@@ -40,5 +72,7 @@ int main() {
       allNodes[i]->rightChild = allNodes[right];
     }
   }
+  printTree(allNodes[0], 0);
+
   return 0;
 }
